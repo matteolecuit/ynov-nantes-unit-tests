f006e4ee34be518160e4b8d3b016ab44c8b05928
mathislrn <mathis.lrn061198@gmail.com>
2022-04-21 13:01:25 +0200

    Workshop add tests to 1-intro/unit-tests/tests/flatten_test.php

--- 1-intro/workshop/unit-tests/tests/flatten_test.php
+++ 1-intro/workshop/unit-tests/tests/flatten_test.php
@@ -6,8 +6,30 @@ require_once('./src/flatten.php');
 
 class FlattenTest extends TestCase
 {
-    public function test_something()
+    public function testFlattenSuccess(): void
     {
-        $this->assertEqual(1337, 1337);
+        $input = [
+            [0, 1],
+            [2, 3],
+            [4, 5],
+            [6, 7],
+            [8, 9],
+        ];
+
+        $result = flatten($input);
+        $expectedResult = range(0, 9);
+
+        $this->assertNotEquals($expectedResult, $input);
+        $this->assertIsArray($result);
+        $this->assertCount(10, $result);
+        $this->assertEquals($expectedResult, $result);
+    }
+
+    public function testFlattenFailure(): void
+    {
+        $input = "[0, [1, 2], [3, 4, 5]]";
+
+        $this->expectException(TypeError::class);
+        flatten($input);
     }
 }
