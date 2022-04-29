bda985aeeb87af6c98a0e6b259bacc6f1de08a17
mathislrn <mathis.lrn061198@gmail.com>
2022-04-29 17:00:57 +0200

    feat: add swagger

--- 4-doc-doc-testing-load-testing/workshop/router.js
+++ 4-doc-doc-testing-load-testing/workshop/router.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const swaggerUi = require('swagger-ui-express');
+const swaggerDocument = require('./swagger.json');
 const { getTodo, postTodo, patchTodo } = require('./toDoController');
 const app = express();
 
@@ -7,8 +9,64 @@ app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
 app.use(express.static(__dirname + '/public'));
 
-app.get('/todo', getTodo);
-app.post('/todo', postTodo);
-app.patch('/todo/:id', patchTodo);
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+
+/**
+ * @swagger
+ * /api/todo:
+ *   get:
+ *     tags: Todo
+ *     description: Updates a single todo
+ *     produces: application/json
+ *     parameters:
+ *       name: todo
+ *       in: body
+ *       description: Fields for the todo resource
+ *       schema:
+ *         type: array
+ *         $ref: '#/definitions/todo'
+ *     responses:
+ *       200:
+ *         description: Successfully updated
+ *   post:
+ *     tags: Todo
+ *     description: Creates a new todo
+ *     produces:
+ *       - application/json
+ *     parameters:
+ *       - name: todo
+ *         description: Puppy object
+ *         in: body
+ *         required: true
+ *         schema:
+ *           $ref: '#/definitions/Puppy'
+ *     responses:
+ *       200:
+ *         description: Successfully created
+ */
+app.route('/todo')
+    .get(getTodo)
+    .post(postTodo);
+
+/**
+ * @swagger
+ * /api/todo/{id}:
+ *   patch:
+ *     tags: Todo
+ *     description: Updates a single todo
+ *     produces: application/json
+ *     parameters:
+ *       name: todo
+ *       in: body
+ *       description: Fields for the todo resource
+ *       schema:
+ *         type: array
+ *         $ref: '#/definitions/todo'
+ *     responses:
+ *       200:
+ *         description: Successfully updated
+ */
+app.route('/todo/:id') 
+    .patch(patchTodo);
 
 module.exports = app;
--- 4-doc-doc-testing-load-testing/workshop/server.js
+++ 4-doc-doc-testing-load-testing/workshop/server.js
@@ -1,8 +1,8 @@
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 5001;
 const router = require("./router");
 
 const mongoose = require("mongoose");
-const ToDo = require("./toDoModel.js").ToDo;
+const { ToDo } = require("./toDoModel.js");
 const DB_URI = "mongodb://mongo:27017/toDoApp";
 
 mongoose.connect(DB_URI).then(() => {
