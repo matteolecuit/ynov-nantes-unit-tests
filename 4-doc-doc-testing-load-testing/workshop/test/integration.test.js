6073319f3ed90d4f702ce1b64a07c8d112931c04
Matteo Lecuit <matteo@lecuit.fr>
2022-04-29 14:48:11 +0200

    should work

--- 4-doc-doc-testing-load-testing/workshop/test/integration.test.js
+++ 4-doc-doc-testing-load-testing/workshop/test/integration.test.js
@@ -1,11 +1,10 @@
 const mongoose = require("mongoose");
 const { ToDo } = require("../toDoModel");
 
-beforeAll(async () => {
-  await mongoose
-    .connect("mongodb://mongo:27017/toDoApp", {
-      useNewUrlParser: true,
-    })
+beforeAll(() => {
+  const DB_URI = "mongodb://mongo:27017/toDoApp";
+  mongoose
+    .connect(DB_URI, { useUnifiedTopology: true })
     .then(() => console.log("MongoDB Connected"))
     .catch((err) => console.log(err));
 });
@@ -15,16 +14,23 @@ const createToDoData = [
   { text: "Super nom", expectedText: "Super nom" },
 ];
 
-describe.each(createToDoData)("Create todo", (data) => {
-  test(`${data.text} should return ${expectedText}`, async () => {
-    const newToDo = new ToDo({ text: data.text });
-    await newToDo.save();
+describe.each(createToDoData)(
+  "Create todo",
+  (data) => {
+    test(`${data.text} should return ${data.expectedText}`, async () => {
+      const newToDo = new ToDo({ text: data.text });
+      await newToDo.save();
 
-    const toDos = await ToDo.find({ text: { $eq: data.text } }, (err, docs) => {
-      if (err) console.log(err);
-      else return docs;
+      const toDos = await ToDo.find(
+        { text: { $eq: data.text } },
+        (err, docs) => {
+          if (err) console.log(err);
+          else return docs;
+        }
+      );
+      expect(toDos.length).toBe(1);
+      expect(toDos[0].text).toBe(data.expectedText);
     });
-    expect(toDos.length).toBe(1);
-    expect(toDos[0].text).toBe(data.expectedText);
-  });
-});
+  },
+  30000
+);
