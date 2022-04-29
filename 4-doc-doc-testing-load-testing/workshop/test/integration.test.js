009782f4b5438de432b8de8baff967beb81bb219
Matteo Lecuit <matteo@lecuit.fr>
2022-04-29 15:07:53 +0200

    test: add integration tests

--- 4-doc-doc-testing-load-testing/workshop/test/integration.test.js
+++ 4-doc-doc-testing-load-testing/workshop/test/integration.test.js
@@ -9,9 +9,10 @@ beforeAll(() => {
     .catch((err) => console.log(err));
 });
 
+const dateData = String(Date.now());
 const createToDoData = [
-  { text: "Nom", expectedText: "Nom" },
-  { text: "Super nom", expectedText: "Super nom" },
+  { text: "Nom" + dateData, expectedText: "Nom" + dateData },
+  { text: "Super nom" + dateData, expectedText: "Super nom" + dateData },
 ];
 
 describe.each(createToDoData)(
@@ -30,6 +31,42 @@ describe.each(createToDoData)(
       );
       expect(toDos.length).toBe(1);
       expect(toDos[0].text).toBe(data.expectedText);
+    });
+  },
+  30000
+);
+
+const createToDoWithDoneData = [
+  {
+    text: "vrai" + dateData,
+    done: true,
+    expectedText: "vrai" + dateData,
+    expectedDone: true,
+  },
+  {
+    text: "faux" + dateData,
+    done: false,
+    expectedText: "faux" + dateData,
+    expectedDone: false,
+  },
+];
+describe.each(createToDoWithDoneData)(
+  "Create todo",
+  (data) => {
+    test(`${data.done} should return ${data.expectedDone}`, async () => {
+      const newToDo = new ToDo({ text: data.text, done: data.done });
+      await newToDo.save();
+
+      const toDos = await ToDo.find(
+        { text: { $eq: data.text } },
+        (err, docs) => {
+          if (err) console.log(err);
+          else return docs;
+        }
+      );
+      expect(toDos.length).toBe(1);
+      expect(toDos[0].text).toBe(data.expectedText);
+      expect(toDos[0].done).toBe(data.expectedDone);
     });
   },
   30000
